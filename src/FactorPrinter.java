5472e05a4a16b04b86c868b3853e86401ec4b55d
xtouskon <xtouskon@gmail.com>
2025-09-25 19:18:22 +0300

    added a class

---
+++ src/FactorPrinter.java
@@ -0,0 +1,18 @@
+public class FactorPrinter {
+    public static void main(String[] args) {
+        printFactors(6);
+    }
+    public static void printFactors(int number){
+        if (number < 1){
+            System.out.println("Invalid Value");
+            return;
+        }
+        for (int i = 1 ; i <= number; i++) {
+            if (number % i == 0){
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
