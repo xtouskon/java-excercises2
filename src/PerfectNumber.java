99b185d7896526fe5c89b5087046bb1585d8448d
xtouskon <xtouskon@gmail.com>
2025-09-29 17:52:53 +0300

    added a class

---
+++ src/PerfectNumber.java
@@ -0,0 +1,17 @@
+public class PerfectNumber {
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(0));
+    }
+    public static boolean isPerfectNumber (int number){
+        if (number <= 0){
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i < number; i++)
+            if (number % i == 0){
+                sum += i;
+            }
+        return sum == number;
+    }
+}
